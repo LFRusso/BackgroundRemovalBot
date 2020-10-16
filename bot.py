455cfb9639fdd83e9f5a2902b187248fe65e57e7
Luiz Fernando Santos <lf.santos@usp.br>
2020-10-16 12:03:33 -0300

    First commit

---
+++ bot.py
@@ -0,0 +1,60 @@
+from telegram.ext import Updater, CommandHandler
+import logging
+import os
+import numpy as np
+from unet import crop as unet
+
+def start(update, context):
+    message = "Hi, @{}! Type /help to see the commands \o/.".format(update.effective_user.username)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+def help(update, context):
+    message = """
+    Comandos: 
+    /help: Display commands
+    /crop: Use the command replying to a photo with a persont in it
+    """    
+    context.bot.send_message(chat_id=update.effective_chat.id, text=message)
+
+# Gets you stick bugged
+def crop(update, context):
+    media = update.message.reply_to_message.photo[0]
+    if (media == None): return
+    if (media.file_size > 184120): return
+    
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Loading...")
+
+    media_id = media.file_id
+    imgFile = context.bot.getFile(media_id)
+
+    fname = media_id
+    imgFile.download(f"tmp/{fname}.jpg")
+    unet.crop_img(fname)
+    os.remove(f"tmp/{fname}.jpg")
+    context.bot.sendPhoto(chat_id=update.effective_chat.id, photo=open(f"tmp/out-{fname}.png", 'rb'))
+    os.remove(f"tmp/out-{fname}.png")
+
+
+
+def main():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(level=logging.INFO,
+                        format="%(asctime)s [%(levelname)s] %(message)s")
+
+
+    TOKEN = os.environ['API_KEY']
+    updater = Updater(token=TOKEN, use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("crop", crop))
+
+    updater.start_polling()
+    logging.info("=== It's alive! ===")
+    updater.idle()
+    logging.info("=== Oh no, It's dying! ===")
+
+
+if __name__ == "__main__":
+    main()
---
+++ unet/crop.py
@@ -0,0 +1,35 @@
+'''
+    U-Net FCN implementation by zhixuhao (https://github.com/zhixuhao/unet) with a few tweaks 
+'''
+from . import model as unet_model
+import skimage.io as io
+import skimage.transform as trans
+import numpy as np
+import matplotlib.pyplot as plt
+import cv2
+
+def prepareImg(image_name,target_size = (256,256),as_gray = True):
+    img = io.imread(image_name,as_gray = as_gray)
+    img = trans.resize(img,target_size)
+    img = np.reshape(img,img.shape+(1,))
+    img = np.reshape(img,(1,)+img.shape)
+    return img
+
+def crop_img(filename):
+    img = prepareImg(f"tmp/{filename}.jpg")
+    model = unet_model.unet()
+    model.load_weights("unet/unet_people.hdf5")
+    result = model.predict(img)[0]
+    result = np.reshape(result, result.shape[:-1])
+    result = np.array(255*result, dtype=np.uint8)
+
+    original_img = cv2.imread(f"tmp/{filename}.jpg")
+    img_mask = cv2.resize(result, (original_img.shape[1],original_img.shape[0]), interpolation = cv2.INTER_AREA)
+    img_mask = cv2.GaussianBlur(img_mask,(7,7),0)
+
+    thresh = 100
+    img_mask[img_mask <= thresh] = 0
+    img_mask[img_mask > thresh] = 1
+    out = cv2.bitwise_and(original_img, original_img, mask=img_mask)
+    out = np.dstack([out, 255*img_mask])
+    cv2.imwrite(f"tmp/out-{filename}.png",out)
